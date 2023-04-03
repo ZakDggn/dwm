3ce2695162b66b9cb88e72b4f98b17bdbbfaf3e5
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2023-04-03 05:58:53 +0100

    Fixed transparent borders with background shown

--- autostart.sh
+++ autostart.sh
@@ -1,10 +1,10 @@
+nitrogen --restore &
 dwmblocks &
 dunst &
 redshift-gtk -l 51:0 &
 picom -b &
 nm-applet &
 setxkbmap -option caps:ctrl_modifier; xcape -e '#66=Escape'
-# nitrogen --restore &
 ~/dwm/low-battery-warning.sh 10 120 &
 earlyoom -r 3600 -n --avoid '(^|/)(init|systemd.*|Xorg|sshd)$' &
 systembus-notify &
