598f94fa52419ac527701abf78a7ec103a43877f
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2023-04-04 15:12:03 +0100

    Increased size of icons in the bar

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
+static const char *fonts[]          = { "JetBrains Mono:size=10", "JetBrainsMono Nerd Font:size=14" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#1a1b26";
 static const char col_gray2[]       = "#444444";
