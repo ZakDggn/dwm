12f2baee787e5af07c164659ebdafd2d19e80a1c
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2023-09-29 17:14:46 +0100

    Added full battery notification, removed setxkbmap from autostart

--- autostart.sh
+++ autostart.sh
@@ -6,8 +6,9 @@ dunst &
 redshift-gtk -l 51:0 &
 picom -b &
 nm-applet &
-setxkbmap -option caps:ctrl_modifier; xcape -e '#66=Escape'
+#setxkbmap -option caps:ctrl_modifier; xcape -e '#66=Escape'
 ~/dwm/low-battery-warning.sh 10 120 &
+~/dwm/full-battery-notification.sh 90 120 &
 earlyoom -r 3600 -n --avoid '(^|/)(init|systemd.*|Xorg|sshd)$' &
 systembus-notify &
 unclutter &
--- full-battery-notification.sh
+++ full-battery-notification.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Low battery threshold and time interval not provided"
+  echo "Full battery threshold and time interval not provided"
   exit 1
 fi
 
 while true; do
   battery_info=$(acpi)
   percentage=$(echo "$battery_info" | awk -F', ' '{sub("%", ""); print $2}')
-  if [ "$percentage" -le "$1" ] && grep -q Discharging <<< "$battery_info"; then
-    notify-send -u critical "Warning: Battery low"
+  if [ "$percentage" -ge $1 ] && grep -q 'Charging\|Full' <<< "$battery_info"; then
+    notify-send "Battery charged"
   fi
   sleep "$2"
 done
