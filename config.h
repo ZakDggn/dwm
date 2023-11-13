1c24486346a2617b8e69581fd8712a023fbaf1f4
ZakDggn <23458039+ZakDggn@users.noreply.github.com>
2023-11-13 01:20:02 +0000

    Adjusted colors, increased font size

--- config.h
+++ config.h
@@ -13,17 +13,18 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=10", "JetBrainsMono Nerd Font:size=14" };
+static const char *fonts[]          = { "JetBrains Mono:size=11", "JetBrainsMono Nerd Font:size=14" };
 static const char dmenufont[]       = "JetBrains Mono:size=10";
-static const char col_gray1[]       = "#1a1b26";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#a9b1d6";
-static const char col_gray4[]       = "#1a1b26";
-static const char col_cyan[]        = "#7aa2f7";
+static const char norm_fg[]         = "#DDDDDD";
+static const char norm_bg[]         = "#11111B";
+static const char norm_border[]     = "#585B70";
+static const char sel_fg[]          = "#11111B";
+static const char sel_bg[]          = "#89B4FA";
+static const char sel_border[]      = "#89B4FA";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg       bg       border     */
+	[SchemeNorm] = { norm_fg, norm_bg, norm_border },
+	[SchemeSel]  = { sel_fg,  sel_bg,  sel_border  },
 };
 
 /* tagging */
@@ -67,7 +68,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
